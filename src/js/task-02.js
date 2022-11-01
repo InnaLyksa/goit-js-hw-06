5837b2725139fec9f4ce496a5d5f977d19d0d3f8
InnaLyksa <in.lyksa@gmail.com>
2022-11-01 20:16:29 +0200

    Task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const itemList = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.textContent = `${ingredient}`;
+  return item;
+});
+// console.log(itemList);
+ingredientsList.append(...itemList);
+console.log(ingredientsList);
