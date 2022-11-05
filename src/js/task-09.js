e7e33095bce9df50996491072e1ac1445986bf97
InnaLyksa <in.lyksa@gmail.com>
2022-11-05 17:03:09 +0200

    Task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const spanColortext = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', onClickBodyChangeColor);
+
+function onClickBodyChangeColor() {
+  const randomBackgroundColor = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = randomBackgroundColor;
+  spanColortext.textContent = randomBackgroundColor;
+}
