4e3b87643bffe782d63dbf9e2b85ab870e321a59
InnaLyksa <in.lyksa@gmail.com>
2022-11-07 12:21:32 +0200

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,8 @@
 const inputEl = document.querySelector('#font-size-control');
 const spanTextEl = document.querySelector('#text');
 
+spanTextEl.style.fontSize = `${inputEl.value}px`;
+
 inputEl.addEventListener('input', onChangeStyleSpanText);
 
 function onChangeStyleSpanText(event) {
